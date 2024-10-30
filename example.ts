a353857f215c18e2c2de1223f2fab3b5c28698ef
DenisPepper <thech@yandex.ru>
2024-10-30 19:43:25 +0300

    add default values

--- example.ts
+++ example.ts
@@ -1,12 +1,19 @@
 import { operators, type Operator } from './derive-types.ts';
 
-export const evaluate = (a: number, b: number, operator: Operator) => {
+type EvaluateProps = {
+  a?: number;
+  b?: number;
+  operator?: Operator;
+};
+
+export const evaluate = (props: EvaluateProps) => {
+  const { a = 1, b = 1, operator = '+' } = props;
   return operators[operator](a, b);
 };
 
-console.log(evaluate(4, 2, '+'));
-console.log(evaluate(4, 2, '*'));
-console.log(evaluate(4, 2, '/'));
+console.log(evaluate({ a: 4, b: 2 }));
+console.log(evaluate({ a: 4, b: 2, operator: '*' }));
+console.log(evaluate({ a: 4, b: 2, operator: '/' }));
 
 // ts-node .\example.ts
 // or use cod-runner
